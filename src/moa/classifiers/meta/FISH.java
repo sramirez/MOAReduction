3093fba805ef1d06e23132cb5f83831f2d4c6016
Sergio R <srg00017@gmail.com>
2016-08-31 16:43:21 +0200

    FISH2 classifier added to the library. First working version.

--- src/moa/classifiers/meta/FISH.java
+++ src/moa/classifiers/meta/FISH.java
@@ -1,20 +1,18 @@
 package moa.classifiers.meta;
 
-import weka.core.Instance;
-import weka.core.Instances;
-
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 import moa.classifiers.AbstractClassifier;
-import weka.classifiers.Classifier;
 import moa.classifiers.lazy.neighboursearch.EuclideanDistance;
-import moa.core.DoubleVector;
 import moa.core.Measurement;
-import moa.options.*;
+import moa.options.ClassOption;
+import moa.options.FloatOption;
+import moa.options.IntOption;
+import weka.classifiers.Classifier;
+import weka.core.Instance;
+import weka.core.Instances;
 
 /**
  * Learning in non-stationary environments.
@@ -32,42 +30,38 @@ import moa.options.*;
  * @version 0.4 (Corrected instance weights in classifier training)
  *
  */
+@SuppressWarnings("serial")
 public class FISH extends AbstractClassifier {
 
     public ClassOption baseLearnerOption = new ClassOption("baseLearner", 'l',
             "Classifier to train.", Classifier.class, "bayes.NaiveBayes");
 
     public IntOption periodOption = new IntOption("period", 'p',
-            "Size of the environments.", 200, 1, Integer.MAX_VALUE);
+            "Size of the environments.", 200, 100, Integer.MAX_VALUE);
 
     public FloatOption distancePropOption = new FloatOption(
             "distanceProportion",
             'd',
             "Distance proportion between space and time varibles.",
-            0.5, 0, 1);
+            1, Float.MIN_VALUE, Float.MAX_VALUE);
     public IntOption kOption = new IntOption("k", 'k',
-            "Number of neighbors.", 3, 1, 100);
+            "Number of neighbors.", 3, 1, 50);
 
-    protected List<Classifier> ensemble;
-    protected List<Double> ensembleWeights;
-    protected List<ArrayList<Double>> bkts, wkts;
+    protected Classifier testClassifier;
     protected List<STInstance> buffer;
-    protected long index;
+    protected long index = 0;
     protected double distanceProp;
     protected int pruning, k, period;
   
 
     @Override
     public void resetLearningImpl() {
-        this.ensemble = new ArrayList<>();
-        this.ensembleWeights = new ArrayList<>();
-        this.bkts = new ArrayList<>();
-        this.wkts = new ArrayList<>();
         this.index = 0;
         this.buffer = null;
         this.period = this.periodOption.getValue();
         this.distanceProp = this.distancePropOption.getValue();
         this.k = this.kOption.getValue();
+        this.testClassifier = ((Classifier) getPreparedClassOption(this.baseLearnerOption));
     }
 
     @Override
@@ -77,49 +71,38 @@ public class FISH extends AbstractClassifier {
         if (this.buffer == null) {
             this.buffer = new LinkedList<STInstance>();
         }
-        this.buffer.add(new STInstance(inst, index));
 
-        if (this.index > this.periodOption.getValue()) {
-            //this.index = 0;
-            double mt = this.buffer.size();
-            Classifier classifier = ((Classifier) getPreparedClassOption(this.baseLearnerOption));
-            
-            // Compute distances (space/time)
-            //double dist[] = new double[buffer.size()];
-            EuclideanDistance distancer = new EuclideanDistance();
-            
-        	List<STInstance> ordered = new LinkedList<STInstance>();
-            for(STInstance sti : buffer) {            	
-            	double distance = distancer.distance(sti.getInstance(), inst) + 
-            			distanceProp * sti.getTime();
-            	sti.setDistance(distance);
-            }
-            
-            Collections.sort(ordered);
-            
-            for (int i = k; i < index + 1 ; i++){
-            	List<STInstance> tra = ordered.subList(0, i);
-            	//train on tra
-            	Instances trai = new Instances(tra.get(0).getInstance().dataset());
-            	for(int z = 1; z < tra.size(); z++) trai.add(tra.get(z).getInstance());
-            	classifier.buildClassifier(trai);
-            	
-            	double errors = 0;
-            	for(int j = 0; j < k ; j++) {
-            		List<STInstance> val = ordered.subList(0, k);
-                	STInstance removed = val.remove(j);
-                	//validate on val  
-                	for(STInstance vinst : val) {
-                		if (classifier.classifyInstance(vinst.getInstance()) != vinst.getInstance().classIndex())
-                			errors++;
-                	}                		
-                	val.add(j, removed);
-            	}
-            	double error = errors / k;
-            }
-        }
+    	this.buffer.add(new STInstance(inst, index));
+        if (this.index < this.periodOption.getValue()) { 
+        	trainClassifier(testClassifier, buffer);
+        }       
         
         this.index++;
+    }
+    
+    private void trainClassifier(Classifier c, List<STInstance> instances) {
+    	Instances trai = new Instances(instances.get(0).getInstance().dataset());
+    	for(int z = 1; z < instances.size(); z++) trai.add(instances.get(z).getInstance());
+		try {
+			c.buildClassifier(trai);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    private double euclideanDistance(Instance i1, Instance i2) throws Exception {
+    	if(i1.numAttributes() != i2.numAttributes())
+			throw new Exception("Euclidean distance performed on uneven instances");
+		
+		double accum = 0;
+		for(int i = 0; i < i1.numAttributes(); i++) {
+			if(i != i1.classIndex()) {
+				accum += Math.pow(i1.value(i) - i2.value(i), 2);
+			}
+		}
+		
+		return Math.sqrt(accum);
     }
 
     @Override
@@ -129,21 +112,85 @@ public class FISH extends AbstractClassifier {
 
     @Override
     public double[] getVotesForInstance(Instance inst) {
-        DoubleVector combinedVote = new DoubleVector();
-        if (this.trainingWeightSeenByModel > 0.0) {
-            for (int i = 0; i < this.ensemble.size(); i++) {
-                if (this.ensembleWeights.get(i) > 0.0) {
-                    DoubleVector vote = new DoubleVector(this.ensemble.get(i)
-                            .getVotesForInstance(inst));
-                    if (vote.sumOfValues() > 0.0) {
-                        vote.normalize();
-                        vote.scaleValues(this.ensembleWeights.get(i));
-                        combinedVote.addValues(vote);
-                    }
-                }
+    	
+    	if (this.index >= this.periodOption.getValue()) {
+        	// The specified size is gotten, now the online process is started
+            Classifier classifier = ((Classifier) getPreparedClassOption(this.baseLearnerOption));
+            
+            // Compute distances (space/time)
+            //EuclideanDistance distancer = new EuclideanDistance();            
+        	for(STInstance sti : buffer) {            	
+            	double distance = Double.MIN_VALUE;
+				try {
+					distance = euclideanDistance(sti.getInstance(), inst) + 
+							distanceProp * sti.getTime();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	sti.setDistance(distance);
             }
-        }
-        return combinedVote.getArrayRef();
+            // Sort instances by time/space distance (ascending)
+            Collections.sort(buffer);
+            
+            List<Double> measurements = new LinkedList<Double>();
+			List<STInstance> val = buffer.subList(0, k);
+            // Apply leave-one-out cross validation (validation set formed by top-k nearest neighbors)
+            for (int i = k; i < buffer.size(); i++){
+            	try {
+	            	List<STInstance> tra = buffer.subList(0, i);
+					Instances instancestr = new Instances(tra.get(0).getInstance().dataset());
+            		for(int z = 1; z < tra.size(); z++) {
+            			instancestr.add(tra.get(z).getInstance());
+            		}
+            		
+	            	double errors = 0;
+	            	for(int j = 0; j < k ; j++) {
+		            	// Validation instance is removed from the training set for this run
+	            		Instance removed = instancestr.remove(j);
+	            		// Train for the tuple i, j
+	            		classifier.buildClassifier(instancestr);
+		            	
+	                	//validate on instance j (from val set)
+	            		int pred = (int) classifier.classifyInstance(val.get(j).getInstance());
+	            		if (pred != val.get(j).getInstance().classValue()) 
+							errors++;
+	                	
+	                	// We add the skipped instance again
+	                	instancestr.add(j, removed);
+	            	}
+                	measurements.add(errors / k);
+	            	
+            	} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }           
+            
+            int min = 0;
+            double minval = Double.MAX_VALUE;
+            for(int i = 0; i < measurements.size(); i++) {
+            	if(measurements.get(i) < minval) {
+            		min = i;
+            		minval = measurements.get(i);
+            	}
+            }
+            //System.out.println("New chosen size: " + (min + k));
+            List<STInstance> lastTrain = buffer.subList(0, min + k);
+            trainClassifier(testClassifier, lastTrain);
+            //buffer = lastTrain;
+    	}
+    	
+    	int pred = 0;
+		try {
+			pred = (int) testClassifier.classifyInstance(inst);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	double[] v = new double[pred + 1];
+    	v[pred] = 1.0;
+    	return v;
     }
 
     @Override
